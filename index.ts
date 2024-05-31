d964e1a964e2f1b02a9e01069d1da5f06793bc9e
muzakka mir <159703338+Muzakka123@users.noreply.github.com>
2024-05-31 07:16:15 +0100

    Add files via upload

---
+++ index.ts
@@ -0,0 +1,42 @@
+import inquirer from "inquirer"
+
+
+const currency: any =  {
+    USD: 1,
+    EUR: 0.91,
+    GBP:0.76,
+    INR:74.57,
+    PKR:280
+}
+ let user_answer:any = await inquirer.prompt(
+    [
+        {
+            name:'from',
+            message:'enter from currency',
+            type:'list',
+            choices:['USD','EUR','INR','GBP','PKR']  
+        },
+        {
+            name:'to',
+            message:'enter from currency',
+            type:'list',
+            choices:['USD','EUR','INR','GBP','PKR']  
+        },
+        {
+            name:'amount',
+            message:"enter your account",
+            type:'number'
+        }
+       
+    ]
+ ) 
+//  console.log(user_answer)
+ let fromAmount = currency [user_answer.from]
+ let toAmount = currency [user_answer.to]
+ let amount = user_answer.amount
+ let baseAmount = amount / fromAmount
+ let convertedAmount = baseAmount *toAmount 
+ console.log( convertedAmount)              
+ console.log(fromAmount)
+ console.log(toAmount)
+ console.log(amount)
